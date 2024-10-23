6d016302a26ddfee417cc7b9c5efe8be30f093b7
SrDouglax <Meioameio8686@gmail.com>
2024-10-23 02:16:23 -0300

    feat: multiprocessing of attempts

--- pyrarcrack.py
+++ pyrarcrack.py
@@ -9,11 +9,13 @@ http://www.enigmagroup.org/code/view/python/168-Rar-password-cracker
 http://rarcrack.sourceforge.net/
 """
 from argparse import ArgumentParser
-from itertools import chain, product
+from itertools import product
 from os.path import exists
+from subprocess import PIPE, Popen, CREATE_NO_WINDOW
+from multiprocessing import Pool, cpu_count
 from string import printable
-from subprocess import PIPE, Popen
 from time import time
+import sys
 
 chars = (
     printable
@@ -22,6 +24,7 @@ chars = (
 special_chars = "();<>`|~\"&\'}]"
 
 parser = ArgumentParser(description='Python combination generator to unrar')
+
 parser.add_argument(
     '--start',
     help='Number of characters of the initial string [1 -> "a", 2 -> "aa"]',
@@ -56,23 +59,35 @@ parser.add_argument('--file', help='.rar file [file.rar]', type=str)
 
 args = parser.parse_args()
 
-
-def generate_combinations(alphabet, length, start=1):
-    """Generate combinations using alphabet."""
-    yield from (
-        ''.join(string)
-        for string in chain.from_iterable(
-            product(alphabet, repeat=x) for x in range(start, length + 1)
-        )
-    )
-
-
-def format(string):
-    """Format chars to write them in shell."""
+def format_string(string):
+    """Formatar caracteres especiais."""
     formated = map(
-        lambda char: char if char not in special_chars else f'\\{char}', string
-    )
+        lambda char: char if char not in special_chars else f'\\{char}', string)
     return ''.join(formated)
+
+
+def generate_combinations(alphabet, start, stop):
+    """Gerar e testar combinações"""
+    for length in range(start, stop + 1):
+        for combination in product(alphabet, repeat=length):
+            yield ''.join(combination)
+
+
+def try_password(combination,z,v,b):
+    """Tentar uma combinação de senha para desbloquear o arquivo .rar."""
+    formated_combination = format_string(combination)
+
+    cmd = Popen(
+        f'{args.unrar_path} t -p{formated_combination} {args.file}'.split(),
+        stdout=PIPE,
+        stderr=PIPE,
+        creationflags=CREATE_NO_WINDOW,
+    )
+    out, err = cmd.communicate()
+
+    if 'All OK' in out.decode():
+        return combination
+    return False
 
 
 if __name__ == '__main__':
@@ -83,22 +98,14 @@ if __name__ == '__main__':
         raise Exception('Stop number is less than start')
 
     start_time = time()
-    for combination in generate_combinations(
-        args.alphabet, args.stop, args.start
-    ):
-        formated_combination = format(combination)
+    # Multiprocessamento para testar combinações
+    print("Started to generate and test combinations...")
+    with Pool(cpu_count()) as pool:
+        for combination in generate_combinations(args.alphabet, args.start, args.stop):
+            result = pool.apply_async(
+                try_password, (combination))
 
-        if args.verbose:
-            print(f'Trying: {combination}')
-
-        cmd = Popen(
-            f'{args.unrar} t -p{formated_combination} {args.file}'.split(),
-            stdout=PIPE,
-            stderr=PIPE,
-        )
-        out, err = cmd.communicate()
-
-        if 'All OK' in out.decode():
-            print(f'Password found: {combination}')
-            print(f'Time: {time() - start_time}')
-            exit()
+            if result.get():
+                print(f'Password found: {result.get()}')
+                print(f'Time: {time() - start_time}')
+                sys.exit(0)
